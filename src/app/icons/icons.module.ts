14f23ef31c2949ead8565b7952d257f823cb5b28
Sergio Ruiz <sergioruizdavila@gmail.com>
2018-03-21 21:48:11 -0500

    Mod: Improved some part of the landing

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IconHelpCircle, IconX, IconCheck, IconHome, IconUsers } from 'angular-feather';
+import { IconHelpCircle, IconX, IconCheck, IconHome, IconUsers, IconArrowRight } from 'angular-feather';
 
 const icons = [
-  IconHelpCircle, IconX, IconCheck, IconHome, IconUsers
+  IconHelpCircle, IconX, IconCheck, IconHome, IconUsers, IconArrowRight
 ];
 
 @NgModule({
